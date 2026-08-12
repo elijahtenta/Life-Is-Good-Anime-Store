0f9262f994495a27f22379c376a9102fb50a1c5e
Eli <69017960+elijahtenta@users.noreply.github.com>
2026-08-11 23:28:54 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <title>Movie Store</title>
+<body>
+  <header>
+    <h1>Welcome to Our Movie Store</h1>
+  </header>
+  
+  <div class="container">
+    <div class="movie-list">
+      <div class="movie-card">
+        <img src="https://img6.arthub.ai/64a007fc-a355.webp" alt="The Matrix Poster" />
+        <div class="movie-card-content">
+          <h2 class="movie-card-title">The Gamer</h2>
+          <p class="movie-card-year">2022</p>
+          <p class="movie-card-price">$19.99</p>
+        </div>
+        <a href="https://youtu.be/mZ1jIY7LMuQ?si=774QP75QYfNKwo_4">Watch Now</a>
+      </div>
+
+      <div class="movie-card">
+        <img src="https://wallpapers.com/images/hd/cute-anime-profile-pictures-0lifptfs0jd9fml3.jpg" alt="The Matrix Poster" />
+        <div class="movie-card-content">
+          <h2 class="movie-card-title">Loner</h2>
+          <p class="movie-card-year">2015</p>
+          <p class="movie-card-price">$17.99</p>
+        </div>
+        <a href="#">Watch Now</a>
+      </div>
+
+      <div class="movie-card">
+        <img src="https://wallpapercave.com/wp/wp7346640.png" alt="The Matrix Poster" />
+        <div class="movie-card-content">
+          <h2 class="movie-card-title">The Matrix</h2>
+          <p class="movie-card-year">1999</p>
+          <p class="movie-card-price">$7.99</p>
+        </div>
+        <a href="#">Watch Now</a>
+      </div>
+
+      <div class="movie-card">
+        <img src="https://pics.craiyon.com/2023-07-14/5de0a040b42245fbac625226ed06acfa.webp" alt="The Matrix Poster" />
+        <div class="movie-card-content">
+          <h2 class="movie-card-title">The Matrix</h2>
+          <p class="movie-card-year">1999</p>
+          <p class="movie-card-price">$7.99</p>
+        </div>
+        <a href="#">Watch Now</a>
+      </div>
+
+      <div class="movie-card">
+        <img src="https://i.pinimg.com/originals/57/3f/22/573f22a1aa17b366f5489745dc4704e1.jpg" alt="The Matrix Poster" />
+        <div class="movie-card-content">
+          <h2 class="movie-card-title">The Matrix</h2>
+          <p class="movie-card-year">1999</p>
+          <p class="movie-card-price">$7.99</p>
+        </div>
+        <a href="#">Watch Now</a>
+      </div>
+
+      <div class="movie-card">
+        <img src="https://wallpaperaccess.com/full/4595683.jpg" alt="The Matrix Poster" />
+        <div class="movie-card-content">
+          <h2 class="movie-card-title">The Matrix</h2>
+          <p class="movie-card-year">1999</p>
+          <p class="movie-card-price">$7.99</p>
+        </div>
+        <a href="#">Watch Now</a>
+      </div>
+
+      <div class="movie-card">
+        <img src="https://wallpapercave.com/wp/wp8546611.jpg" alt="The Matrix Poster" />
+        <div class="movie-card-content">
+          <h2 class="movie-card-title">The Matrix</h2>
+          <p class="movie-card-year">1999</p>
+          <p class="movie-card-price">$7.99</p>
+        </div>
+        <a href="#">Watch Now</a>
+      </div>
+
+      <div class="movie-card">
+        <img src="https://wallpapers.com/images/hd/anime-meme-pfp-shiba-dog-naruto-akatsuki-qg4jwiz9wanor4vf.jpg" alt="The Matrix Poster" />
+        <div class="movie-card-content">
+          <h2 class="movie-card-title">The Matrix</h2>
+          <p class="movie-card-year">1999</p>
+          <p class="movie-card-price">$7.99</p>
+        </div>
+        <a href="#">Watch Now</a>
+      </div>
+
+    </div>
+</div>  
+</body>
+    </div>
+  );
+  }
+
+export default App;
